ac3b26ec10a3f1e9cc7eb3473c4cd1b9ca0cae37
Kes Johnson <kes.j.johnson@gmail.com>
2022-03-14 11:27:49 -0400

    added apps for the different parts

--- toolbox/apps.py
+++ toolbox/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DiloConfig(AppConfig):
+class ToolboxConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dl-app'
+    name = 'toolbox'
--- workspace/views.py
+++ workspace/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the dl-app index.")
+    return HttpResponse("Hello, world. You're at the workspace index.")
