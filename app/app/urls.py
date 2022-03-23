27fb48883998b9913270c97bf9bfb2d0fe13ead3
rhiles1 <roberthiles19@gmail.com>
2022-03-23 17:48:44 -0400

    logism

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,4 +1,4 @@
-"""DeltaLambdaSigma URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
+from django.template.defaulttags import url
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index),
 ]
